6546e9bfeb612225a94b93ecb2b188255e697055
Sally az <sallyazulay@gmail.com>
2023-08-02 13:37:40 +0300

    dimentions helper

---
+++ utils/dimensionsHelper.ts
@@ -0,0 +1,12 @@
+import { Dimensions } from "react-native";
+
+
+export const tileDimension = Dimensions.get('window').width * 0.45;
+export const tileMarginDimension =  tileDimension / 8;
+export const searchDimensions = Dimensions.get('window').width * 0.95;
+
+// Weather component
+export const weatherTile = Dimensions.get('window').width * 0.8;
+export const weatherContainer = Dimensions.get('window').width * 0.35;
+export const weatherContainerHeight = Dimensions.get('window').height * 0.16;
+
